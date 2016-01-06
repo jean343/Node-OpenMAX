208da2d312be834657047e6d1a950cf662fe16b3
JP <bergeron.jeanphilippe@gmail.com>
2016-01-05 21:17:09 -0500

    Update sample SimpleVideoDecoderRenderBuffer

--- examples/SimpleVideoDecoderRenderBuffer.ts
+++ examples/SimpleVideoDecoderRenderBuffer.ts
@@ -2,41 +2,43 @@ import fs = require('fs');
 import stream = require('stream');
 import omx = require('../');
 
-/*class Component extends stream.Duplex{
-
-}
-
-TransformFilter._read = function () {
-};
-TransformFilter._write = function (chunk, enc, next) {
-
-  // Write a black square 200 x 200 in the Y channel of th YUV stream
-  for (var x = 0; x < 200; x++) {
-    for (var y = 0; y < 200; y++) {
-      chunk.writeUInt8(0x0, y * 1920 + x);
-    }
+class TransformFilter extends stream.Duplex {
+  constructor() {
+    super();
+    var self = this;
+    // Needed to forward the portDefinitionChanged from the VideoDecode to the VideoRender
+    this.on('pipe', function (source) {
+      source.on('portDefinitionChanged', function (portDefinition) {
+        self.emit('portDefinitionChanged', portDefinition);
+      });
+    });
   }
+  _read  () {
+  };
+  _write (chunk, enc, next) {
 
-  this.push(chunk);
+    // Write a black square 200 x 200 in the Y channel of th YUV stream
+    for (var x = 0; x < 200; x++) {
+      for (var y = 0; y < 200; y++) {
+        chunk.writeUInt8(0x0, y * 1920 + x);
+      }
+    }
 
-  next();
-};
-// Needed to forward the portDefinitionChanged from the VideoDecode to the VideoRender
-TransformFilter.on('pipe', function (source) {
-  var self = this;
-  source.on('portDefinitionChanged', function (portDefinition) {
-    self.emit('portDefinitionChanged', portDefinition);
-  });
-});*/
+    this.push(chunk);
+
+    next();
+  };
+}
 
 var VideoDecode = new omx.VideoDecode();
 var VideoRender = new omx.VideoRender();
+var transformFilter = new TransformFilter();
 
 VideoDecode.setVideoPortFormat(omx.OMX_VIDEO_CODINGTYPE.OMX_VIDEO_CodingAVC);
 
 fs.createReadStream("spec/video-LQ.h264")
     .pipe(VideoDecode)
-    //.pipe(TransformFilter)
+    .pipe(transformFilter)
     .pipe(VideoRender)
     .on('finish', function () {
       console.log("Done");
