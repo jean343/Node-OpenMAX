d668717600fdaa1291f158cf6be9d3c7a478e720
Jean-Philippe Bergeron <bergeron.jeanphilippe@gmail.com>
2016-01-23 21:00:52 -0500

    up

--- examples/Perf.ts
+++ examples/Perf.ts
@@ -2,12 +2,42 @@ import fs = require('fs');
 import omx = require('../');
 import stream = require('stream');
 
-var buf: Buffer = new Buffer(3133440);
+var bufferFormat = {
+  eDir: 1,
+  nBufferCountActual: 1,
+  nBufferCountMin: 1,
+  nBufferSize: 3133440,
+  bEnabled: 0,
+  bPopulated: 0,
+  eDomain: 1,
+  video:
+  {
+    pNativeRender: false,
+    nFrameWidth: 1920,
+    nFrameHeight: 1080,
+    nStride: 1920,
+    nSliceHeight: 1088,
+    nBitrate: 0,
+    xFramerate: 0,
+    bFlagErrorConcealment: 0,
+    eCompressionFormat: 0,
+    eColorFormat: 20,
+    pNativeWindow: false
+  }
+};
+var bufferFormatSize = bufferFormat.video.nStride * bufferFormat.video.nSliceHeight;
+
+var buf: Buffer = new Buffer(bufferFormat.nBufferSize);
+
+buf.fill(0, 0, bufferFormatSize);
+buf.fill(128, bufferFormatSize,
+  bufferFormatSize + 2 * bufferFormatSize / 4);
 
 class WritableFilter extends stream.Writable {
   fps;
   portDefinition;
   nStride;
+  nSliceHeight;
   constructor(
     public name: string,
     public offsetX: number,
@@ -30,22 +60,28 @@ class WritableFilter extends stream.Writable {
         //        console.log('portDefinitionChanged', portDefinition);
         self.portDefinition = portDefinition;
         self.nStride = self.portDefinition.video.nStride;
+        self.nSliceHeight = self.portDefinition.video.nSliceHeight;
       });
     });
   }
   _write(chunk, enc, next) {
     //    console.log('_write length', chunk.length, chunk);
+    if (this.portDefinition.video.eColorFormat === omx.OMX_COLOR_FORMATTYPE.OMX_COLOR_FormatYUV420PackedPlanar) {
+      var w = Math.min(this.nStride, this.width);
+      var h = Math.min(this.nSliceHeight, this.height);
 
-    var w = Math.min(this.nStride, this.width);
-    var h = Math.min(this.portDefinition.video.nSliceHeight, this.height);
+      function copyBlock(offsetIn, offsetOut, offsetX, offsetY, nStride, _w, _h, _width) {
+        for (var y = 0; y < _h; y++) {
+          var s = chunk.slice(offsetIn + y * nStride, offsetIn + y * nStride + _w);
 
-    for (var y = 0; y < h; y++) {
-      var s = chunk.slice(y * this.nStride, y * this.nStride + w);
+          var destOffset = offsetOut + offsetX + (offsetY + y) * _width;
+          s.copy(buf, destOffset);
+        }
+      }
 
-      var destOffset = this.offsetX + (this.offsetY + y) * width;
-      s.copy(buf, destOffset);
+      copyBlock(0, 0, this.offsetX, this.offsetY, this.nStride, w, h, width);
+      copyBlock(this.nStride * this.nSliceHeight, bufferFormatSize, this.offsetX / 2, this.offsetY / 2, this.nStride / 2, w / 2, h / 2, width / 2);
     }
-
     this.fps.tick();
     next();
   };
@@ -67,29 +103,7 @@ class ReadableFilter extends stream.Readable {
 
     if (this.firstPacket) {
       this.firstPacket = false;
-      this.emit('portDefinitionChanged', {
-        eDir: 1,
-        nBufferCountActual: 1,
-        nBufferCountMin: 1,
-        nBufferSize: 3133440,
-        bEnabled: 0,
-        bPopulated: 0,
-        eDomain: 1,
-        video:
-        {
-          pNativeRender: false,
-          nFrameWidth: 1920,
-          nFrameHeight: 1080,
-          nStride: 1920,
-          nSliceHeight: 1088,
-          nBitrate: 0,
-          xFramerate: 0,
-          bFlagErrorConcealment: 0,
-          eCompressionFormat: 0,
-          eColorFormat: 20,
-          pNativeWindow: false
-        }
-      });
+      this.emit('portDefinitionChanged', bufferFormat);
     }
 
     setTimeout(function() {
@@ -104,8 +118,8 @@ class ReadableFilter extends stream.Readable {
 // row |
 // row |
 
-var rows = 2; // y
-var cols = 2; // x
+var rows = 1; // y
+var cols = 1; // x
 var width = 1920;
 var height = 1080;
 
@@ -122,8 +136,8 @@ function loop() {
   var rx = coli * rw;
   var ry = rowi * rh;
 
-  var ws = new WritableFilter('count' + i, rx, ry, rw, rh);
-  //  var ws = new WritableFilter('count' + i, 100, 100, 640, 480);
+  //  var ws = new WritableFilter('count' + i, rx, ry, rw, rh);
+  var ws = new WritableFilter('count' + i, 0, 0, 640, 480);
   VideoDecode.init()
     .then(function() {
       VideoDecode.setVideoPortFormat(omx.OMX_VIDEO_CODINGTYPE.OMX_VIDEO_CodingAVC);
