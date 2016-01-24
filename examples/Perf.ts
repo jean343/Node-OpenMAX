62448e57d4cbeb186be4c56d8f92a689c202c619
Jean-Philippe Bergeron <bergeron.jeanphilippe@gmail.com>
2016-01-24 15:33:15 -0500

    adding Resize component

--- examples/Perf.ts
+++ examples/Perf.ts
@@ -57,10 +57,10 @@ class WritableFilter extends stream.Writable {
 
     this.on('pipe', function(source) {
       source.on('portDefinitionChanged', function(portDefinition) {
-        //        console.log('portDefinitionChanged', portDefinition);
+        console.log('portDefinitionChanged', portDefinition);
         self.portDefinition = portDefinition;
-        self.nStride = self.portDefinition.video.nStride;
-        self.nSliceHeight = self.portDefinition.video.nSliceHeight;
+        self.nStride = self.portDefinition.image.nStride;
+        self.nSliceHeight = self.portDefinition.image.nSliceHeight;
       });
     });
   }
@@ -80,7 +80,7 @@ class WritableFilter extends stream.Writable {
     }
   }
   _write(chunk, enc, next) {
-    if (this.portDefinition.video.eColorFormat === omx.OMX_COLOR_FORMATTYPE.OMX_COLOR_FormatYUV420PackedPlanar) {
+    if (this.portDefinition.image.eColorFormat === omx.OMX_COLOR_FORMATTYPE.OMX_COLOR_FormatYUV420PackedPlanar) {
       var w = Math.min(this.nStride, this.width);
       var h = Math.min(this.nSliceHeight, this.height);
 
@@ -136,40 +136,42 @@ class WriteFileFilter extends stream.Duplex {
   };
 }
 
-//class WriteHTTP extends stream.Duplex {
-//  http = require('http');
-//  res = null;
-//  onConnection;
-//  constructor() {
-//    super();
-//    var self = this;
-//    this.http.createServer(function(req, res) {
-//      console.log('createServer');
-//      res.writeHead(200, { 'Content-Type': 'Video/H264' });
-//      self.res = res;
-//      self.onConnection();
-//    }).listen(3000);
-//  }
-//  _read() {
-//  };
-//  _write(chunk, enc, next) {
-//    this.res.write(chunk, next);
-//  };
-//}
+class WriteHTTP extends stream.Duplex {
+  http = require('http');
+  res = null;
+  onConnection;
+  constructor() {
+    super();
+    var self = this;
+    this.http.createServer(function(req, res) {
+      console.log('createServer');
+      res.writeHead(200, { 'Content-Type': 'Video/H264' });
+      self.res = res;
+      self.onConnection();
+    }).listen(3000);
+  }
+  _read() {
+  };
+  _write(chunk, enc, next) {
+    this.res.write(chunk);
+    next();
+  };
+}
 
 //     | col | col
 // row |
 // row |
 
 (function() {
-  var rows = 1; // y
-  var cols = 2; // x
+  var rows = 2; // y
+  var cols = 4; // x
   var width = 1920;
   var height = 1080;
 
   var i = 1;
   function loop() {
     var VideoDecode: omx.VideoDecode;
+    var Resize: omx.Resize;
     VideoDecode = new omx.VideoDecode('VideoDecode' + i);
 
     var coli = (i - 1) % cols;
@@ -183,11 +185,40 @@ class WriteFileFilter extends stream.Duplex {
     var ws = new WritableFilter('count' + i, rx, ry, rw, rh);
     VideoDecode.init()
       .then(function() {
+        Resize = new omx.Resize();
+        return Resize.init();
+      })
+      .then(function() {
         VideoDecode.setVideoPortFormat(omx.OMX_VIDEO_CODINGTYPE.OMX_VIDEO_CodingAVC);
         VideoDecode.setBufferCount(1);
 
+        var format = {
+          eDir: 1,
+          nBufferCountActual: 1,
+          nBufferCountMin: 1,
+          nBufferSize: 460800,
+          bEnabled: 0,
+          bPopulated: 0,
+          eDomain: 2,
+          image:
+          {
+            pNativeRender: false,
+            nFrameWidth: rw,
+            nFrameHeight: rh,
+            nStride: Math.ceil(rw / 16) * 16,
+            nSliceHeight: Math.ceil(rh / 16) * 16,
+            bFlagErrorConcealment: 0,
+            eCompressionFormat: 0,
+            eColorFormat: 20,
+            pNativeWindow: false
+          }
+        };
+        console.log(format);
+        Resize.setParameter(Resize.out_port, omx.OMX_INDEXTYPE.OMX_IndexParamPortDefinition, format);
+
         fs.createReadStream("spec/data/video-LQ-640.h264")
           .pipe(VideoDecode)
+          .pipe(Resize)
           .pipe(ws)
           .on('finish', function() {
             console.log("Done");
@@ -203,36 +234,42 @@ class WriteFileFilter extends stream.Duplex {
 
 
   var VideoEncode: omx.VideoEncode;
-  //  var VideoRender: omx.VideoRender;
+  var VideoRender: omx.VideoRender;
   var readableFilter = new ReadableFilter();
   var writeFileFilter = new WriteFileFilter();
+  var writeHTTP = new WriteHTTP();
 
   VideoEncode = new omx.VideoEncode();
   VideoEncode.init()
-    //    .then(function() {
-    //      VideoRender = new omx.VideoRender();
-    //      return VideoRender.init();
-    //    })
     .then(function() {
-      VideoEncode.setVideoPortFormat(omx.OMX_VIDEO_CODINGTYPE.OMX_VIDEO_CodingAVC);
+      VideoRender = new omx.VideoRender();
+      return VideoRender.init();
+    })
+    .then(function() {
+      //      VideoEncode.setVideoPortFormat(omx.OMX_VIDEO_CODINGTYPE.OMX_VIDEO_CodingAVC);
+      //
+      //      VideoEncode.component.setParameter(VideoEncode.out_port, omx.OMX_INDEXTYPE.OMX_IndexParamVideoBitrate, {
+      //        eControlRate: omx.OMX_VIDEO_CONTROLRATETYPE.OMX_Video_ControlRateDisable
+      //      });
+      //
+      //      var quantizationType = VideoEncode.component.getParameter(VideoEncode.out_port, omx.OMX_INDEXTYPE.OMX_IndexParamVideoQuantization);
+      //      quantizationType.nQpI = 25;
+      //      quantizationType.nQpP = quantizationType.nQpI;
+      //      VideoEncode.component.setParameter(VideoEncode.out_port, omx.OMX_INDEXTYPE.OMX_IndexParamVideoQuantization, quantizationType);
 
-      VideoEncode.component.setParameter(VideoEncode.out_port, omx.OMX_INDEXTYPE.OMX_IndexParamVideoBitrate, {
-        eControlRate: omx.OMX_VIDEO_CONTROLRATETYPE.OMX_Video_ControlRateDisable
-      });
-
-      var quantizationType = VideoEncode.component.getParameter(VideoEncode.out_port, omx.OMX_INDEXTYPE.OMX_IndexParamVideoQuantization);
-      quantizationType.nQpI = 25;
-      quantizationType.nQpP = quantizationType.nQpI;
-      VideoEncode.component.setParameter(VideoEncode.out_port, omx.OMX_INDEXTYPE.OMX_IndexParamVideoQuantization, quantizationType);
-
-      var http = require('http');
-      http.createServer(function(req, res) {
-        console.log('createServer');
-        readableFilter
-          .pipe(VideoEncode)
-          .pipe(res);
-        loop();
-      }).listen(3000);
+      //      var http = require('http');
+      //      http.createServer(function(req, res) {
+      //        console.log('createServer');
+      //        readableFilter
+      //          .pipe(VideoEncode)
+      //          .pipe(res);
+      //        loop();
+      //      }).listen(3000);
+      
+      readableFilter
+        //        .pipe(VideoEncode)
+        .pipe(VideoRender);
+      loop();
 
     });
 
